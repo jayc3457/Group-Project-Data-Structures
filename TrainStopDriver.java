896467ec0fa879ba625bda1f6811e4c9e1626cab
jayc3457 <56646955+jayc3457@users.noreply.github.com>
2019-12-01 00:06:55 -0600

    Update TrainStopDriver.java

--- TrainStopDriver.java
+++ TrainStopDriver.java
@@ -1,6 +1,27 @@
-public class TrainStopDriver {
-    public static void main(String[] args) {
-        TrainStop trainStop = new TrainStop("Jesse", 1);
-        System.out.println(trainStop);
+/******************************************************************************
+* TrainStopDriver.java
+* by Jay Canty
+* 
+* 
+******************************************************************************/
+
+public class TrainStopDriver 
+{
+  public static void main(String[] args) 
+  {
+    TrainRoute route = new TrainRoute();
+    TrainStop[] stops = {new TrainStop("Jesse", 1), new TrainStop("Manish", 4), 
+      new TrainStop("Jay", 8), new TrainStop("Cross Roads", 6), new TrainStop("Downtown", 0),
+      new TrainStop("Westport", 5)};
+    
+    for (TrainStop ts : stops)
+    {
+      route.add(ts);
     }
-}
+    
+    route.printStops();
+    route.getRoute("Westport", "Jesse");
+    route.getRoute("Manish", "Crossroads";
+    route.getRoute("Downtoen", "Jay");
+  } // end main
+} // end TrainStopDriver
